4f0b91a0ea834a5da001e4617399af56199a5395
Wellington Mendoza <wellsaint91@gmail.com>
2024-08-19 17:23:17 +0200

    Update home/.config/fish/conf.d/fnm.fish

--- home/dot_config/fish/conf.d/fnm.fish
+++ home/dot_config/fish/conf.d/fnm.fish
@@ -1,2 +1,2 @@
 # see: https://github.com/Schniz/fnm/issues/356
-status is-interactive && command -q fnm && fnm env --use-on-cd | source
+status is-interactive && command -q fnm && fnm env --use-on-cd --shell fish | source
