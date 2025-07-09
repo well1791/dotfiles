b78a0fa3ee75ccd051a2787372d6988f2197781e
Wellington Mendoza <wellsaint91@gmail.com>
2025-07-09 09:02:30 +0200

    update configs for linux

--- home/dot_config/fish/conf.d/fnm.fish
+++ home/dot_config/fish/conf.d/fnm.fish
@@ -1,2 +1,9 @@
 # see: https://github.com/Schniz/fnm/issues/356
 status is-interactive && command -q fnm && fnm env --use-on-cd --shell fish | source
+
+# fnm
+set FNM_PATH "/home/well/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 # default
 set -g fish_greeting
-set -gx EDITOR /usr/local/bin/hx
-set -gx VISUAL /usr/local/bin/hx
+#set -gx EDITOR /usr/local/bin/hx
+#set -gx VISUAL /usr/local/bin/hx
 set -gx LESS "--no-init --RAW-CONTROL-CHARS --ignore-case --quit-if-one-screen --silent --tabs=4 --window=-2"
 
 fish_add_path -m ~/.local/bin
