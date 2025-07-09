c3cbec88a746386eb5035f8015d741f430317154
Wellington Mendoza <wellsaint91@gmail.com>
2025-07-09 22:19:16 +0200

    update dotfiles

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,8 +1,23 @@
 # default
 set -g fish_greeting
-#set -gx EDITOR /usr/local/bin/hx
-#set -gx VISUAL /usr/local/bin/hx
 set -gx LESS "--no-init --RAW-CONTROL-CHARS --ignore-case --quit-if-one-screen --silent --tabs=4 --window=-2"
+
+# Try common helix binary names in common locations
+set helix_paths /usr/local/bin/hx /usr/local/bin/helix /usr/bin/hx /usr/bin/helix ~/.cargo/bin/hx
+
+for path in $helix_paths
+    if test -x $path
+        set -gx EDITOR $path
+        set -gx VISUAL $path
+        break
+    end
+end
+
+if not set -q EDITOR
+    echo "Warning: Could not find helix editor" >&2
+    set -gx EDITOR vim
+    set -gx VISUAL vim
+end
 
 fish_add_path -m ~/.local/bin
 
