156c1f069f00067f08c946c682573e6a5fa033bc
Wellington Mendoza <wellsaint91@gmail.com>
2025-08-04 17:57:10 +0200

    fish(abbrs): add abbr for kanata config

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -10,3 +10,4 @@ abbr -a mee 'chezmoi edit'
 abbr -a me --set-cursor 'chezmoi edit ~/.config/%'
 abbr -a -- mj chezmoi edit ~/.config/zellij/config.kdl
 abbr -a -- mh chezmoi edit ~/.config/helix/config.toml
+abbr -a -- mk chezmoi edit ~/.config/kanata/linux-qwerty.kbd
