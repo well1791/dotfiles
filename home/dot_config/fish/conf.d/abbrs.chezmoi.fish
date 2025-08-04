499475658591def7be41723be146f468e11566b5
Wellington Mendoza <wellsaint91@gmail.com>
2025-08-04 15:02:39 +0200

    fish(abbrs): add abbr for `chezmoi edit`

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -6,6 +6,7 @@ abbr -a -- ms chezmoi status
 abbr -a -- mc chezmoi cd
 
 # |-- EDIT
+abbr -a mee 'chezmoi edit'
 abbr -a me --set-cursor 'chezmoi edit ~/.config/%'
 abbr -a -- mj chezmoi edit ~/.config/zellij/config.kdl
 abbr -a -- mh chezmoi edit ~/.config/helix/config.toml
