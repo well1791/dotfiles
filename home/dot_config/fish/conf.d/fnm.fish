86cfc3c4e6698aeec410d60adb4bdd9a5840450d
Wellington Mendoza <wellsaint91@gmail.com>
2025-09-08 23:39:32 +0200

    update: many things

--- home/dot_config/fish/conf.d/fnm.fish
+++
@@ -1,9 +0,0 @@
-# see: https://github.com/Schniz/fnm/issues/356
-status is-interactive && command -q fnm && fnm env --use-on-cd --shell fish | source
-
-# fnm
-set FNM_PATH "$HOME/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-    set PATH "$FNM_PATH" $PATH
-    fnm env | source
-end
