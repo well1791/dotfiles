41e074a8d132fcf8a588613f1e54904062be3463
Wellington Mendoza <wellsaint91@gmail.com>
2025-09-22 18:44:00 +0200

    fish(abbrs): add abbrs for wezterm and other things

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -8,6 +8,8 @@ abbr -a -- mc chezmoi cd
 # |-- EDIT
 abbr -a mee 'chezmoi edit'
 abbr -a me --set-cursor 'chezmoi edit ~/.config/%'
+abbr -a -- mf chezmoi edit ~/.config/fish
 abbr -a -- mj chezmoi edit ~/.config/zellij/config.kdl
 abbr -a -- mh chezmoi edit ~/.config/helix/config.toml
 abbr -a -- mk chezmoi edit ~/.config/kanata/config.kbd
+abbr -a -- mw chezmoi edit ~/.config/wezterm/wezterm.lua
