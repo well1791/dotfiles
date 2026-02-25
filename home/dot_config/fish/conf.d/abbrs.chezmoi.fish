4d71c824a42f1f74c5a57fd69e22c5efbf7e09cd
Wellington Mendoza <wellsaint91@gmail.com>
2026-02-25 15:37:02 +0100

    fish: add abbr for ghostty config

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -8,7 +8,7 @@ abbr -a -- mc chezmoi cd
 # |-- EDIT
 abbr -a mee --set-cursor 'chezmoi edit ~/%'
 abbr -a me --set-cursor 'chezmoi edit ~/.config/%'
-abbr -a -- mf chezmoi edit ~/.config/fish
+abbr -a mf --set-cursor 'chezmoi edit ~/.config/fish/%'
 abbr -a -- mh chezmoi edit ~/.config/helix/config.toml
 abbr -a -- mk chezmoi edit ~/.config/kanata/config.kbd
-abbr -a -- mw chezmoi edit ~/.config/wezterm/wezterm.lua
+abbr -a -- mg chezmoi edit ~/.config/ghostty/config
