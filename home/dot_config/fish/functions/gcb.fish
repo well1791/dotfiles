501564334c7e5e75f884b33cd9c077a7eff6572c
Wellington Mendoza <wellsaint91@gmail.com>
2026-02-25 15:36:32 +0100

    git: add another worktree to git-clone-bare custom function

--- home/dot_config/fish/functions/gcb.fish
+++ home/dot_config/fish/functions/gcb.fish
@@ -16,6 +16,7 @@ function gcb --description "Clones a repo as bare and set up two worktrees."
     git config --local --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
     git worktree add -b rename1 wt/1/$proj_name
     git worktree add -b rename2 wt/2/$proj_name
+    git worktree add -b rename3 wt/3/$proj_name
 
     cd wt/1/$proj_name
 end
