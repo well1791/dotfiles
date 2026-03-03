26508e818f7431a2df2eba157783029e1fab90db
Wellington Mendoza <wellsaint91@gmail.com>
2026-03-03 15:56:27 +0100

    fish(abbrs): use chezmoi with --watch flag

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -6,9 +6,9 @@ abbr -a -- ms chezmoi status
 abbr -a -- mc chezmoi cd
 
 # |-- EDIT
-abbr -a mee --set-cursor 'chezmoi edit ~/%'
-abbr -a me --set-cursor 'chezmoi edit ~/.config/%'
-abbr -a mf --set-cursor 'chezmoi edit ~/.config/fish/%'
-abbr -a -- mh chezmoi edit ~/.config/helix/config.toml
-abbr -a -- mk chezmoi edit ~/.config/kanata/config.kbd
-abbr -a -- mg chezmoi edit ~/.config/ghostty/config
+abbr -a mee --set-cursor 'chezmoi edit --watch ~/%'
+abbr -a me --set-cursor 'chezmoi edit --watch ~/.config/%'
+abbr -a mf --set-cursor 'chezmoi edit --watch ~/.config/fish/%'
+abbr -a -- mh chezmoi edit --watch ~/.config/helix/config.toml
+abbr -a -- mk chezmoi edit --watch ~/.config/kanata/config.kbd
+abbr -a -- mg chezmoi edit --watch ~/.config/ghostty/config
