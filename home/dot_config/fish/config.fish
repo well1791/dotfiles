d5a6c7876267e40b0f53c095e38286ac5a31984c
Wellington Mendoza <wellsaint91@gmail.com>
2026-04-27 16:02:45 +0200

    fish: set sudoedit

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -2,6 +2,7 @@
 set -g fish_greeting
 set -gx LESS "--no-init --RAW-CONTROL-CHARS --ignore-case --quit-if-one-screen --silent --tabs=4 --window=2"
 set -g XDG_CONFIG_HOME "$HOME/.config"
+set -g SUDO_EDITOR helix
 
 fish_add_path -m ~/.local/bin
 
