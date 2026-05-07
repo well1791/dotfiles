25d13522ebccf75a2e2bbe064573af62dff84839
Wellington Mendoza <wellsaint91@gmail.com>
2026-05-07 15:59:56 +0200

    fish(abbr): add docs on some abbrs

--- home/dot_config/fish/conf.d/abbrs.fish
+++ home/dot_config/fish/conf.d/abbrs.fish
@@ -8,7 +8,15 @@ abbr -a -- cat 'bat -p'
 # |-- EMACS
 # abbr -a -- emacs "emacsclient -c -a 'emacs'"
 
-# |-- OTHERS
+# |-- ALIAS for cmd
 abbr -a -- cmd command
+
+# |-- Search word in file
+# | usage:
+# |        rgd text-to-search path/to/file
 abbr -a rgd --set-cursor 'rg --json -C 2 % | delta'
-abbr -a q --set-cursor 'pi --model big-pickle -p "%"'
+
+# |-- Question to big-pickle
+# | usage:
+# |        q "question?"
+abbr -a q --set-cursor 'pi --no-session --model big-pickle -p "%"'
