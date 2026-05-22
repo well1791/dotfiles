d77ea07b51b8ea85a19c9a4ec53fdf9940224be2
Wellington Mendoza <wellsaint91@gmail.com>
2026-05-22 17:09:58 +0200

    chore(abbrs): add/update more chezmoi abbrs

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -1,6 +1,7 @@
 # |-- CHEZMOI
 abbr -a -- m chezmoi
-abbr -a -- ma chezmoi add
+abbr -a -- my chezmoi apply
+abbr -a -- ma chezmoi re-add
 abbr -a -- md chezmoi diff
 abbr -a -- ms chezmoi status
 abbr -a -- mc chezmoi cd
@@ -12,4 +13,4 @@ abbr -a mf --set-cursor 'chezmoi edit --watch ~/.config/fish/%'
 abbr -a -- mh chezmoi edit --watch ~/.config/helix/config.toml
 abbr -a -- mk chezmoi edit --watch ~/.config/kanata/config.kbd
 abbr -a -- mg chezmoi edit --watch ~/.config/ghostty/config
-abbr -a -- mj chezmoi edit --watch ~/.config/zellij/config.kdl
+abbr -a -- mz chezmoi edit --watch ~/.config/zellij/config.kdl
