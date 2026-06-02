37a3485d1e9b1243c12ac587afb1d5e2594b2d00
Wellington Mendoza <wellsaint91@gmail.com>
2026-06-02 15:16:47 +0200

    chore(chezmoi): update abbrs

--- home/dot_config/fish/conf.d/abbrs.chezmoi.fish
+++ home/dot_config/fish/conf.d/abbrs.chezmoi.fish
@@ -1,16 +1,17 @@
 # |-- CHEZMOI
 abbr -a -- m chezmoi
-abbr -a -- my chezmoi apply
-abbr -a -- ma chezmoi re-add
-abbr -a -- md chezmoi diff
-abbr -a -- ms chezmoi status
+abbr -a -- mly chezmoi apply
+abbr -a -- ma chezmoi add
+abbr -a -- maa chezmoi re-add
+abbr -a -- mdi chezmoi diff
+abbr -a -- mst chezmoi status
 abbr -a -- mc chezmoi cd
 
 # |-- EDIT
-abbr -a mee --set-cursor 'chezmoi edit --watch ~/%'
-abbr -a me --set-cursor 'chezmoi edit --watch ~/.config/%'
-abbr -a mf --set-cursor 'chezmoi edit --watch ~/.config/fish/%'
-abbr -a -- mh chezmoi edit --watch ~/.config/helix/config.toml
-abbr -a -- mk chezmoi edit --watch ~/.config/kanata/config.kbd
-abbr -a -- mg chezmoi edit --watch ~/.config/ghostty/config
-abbr -a -- mz chezmoi edit --watch ~/.config/zellij/config.kdl
+abbr -a me --set-cursor 'chezmoi edit --watch ~/%'
+abbr -a mee --set-cursor 'chezmoi edit --watch ~/.config/%'
+abbr -a mfi --set-cursor 'chezmoi edit --watch ~/.config/fish/%'
+abbr -a -- mhx chezmoi edit --watch ~/.config/helix/config.toml
+abbr -a -- mkn chezmoi edit --watch ~/.config/kanata/config.kbd
+abbr -a -- mgt chezmoi edit --watch ~/.config/ghostty/config
+abbr -a -- mzj chezmoi edit --watch ~/.config/zellij/config.kdl
