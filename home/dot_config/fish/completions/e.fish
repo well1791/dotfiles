97aea201150bbc4c484e117f9ac52ebfeead7a56
Wellington Mendoza <wellsaint91@gmail.com>
2026-06-10 13:42:05 +0200

    refactor(e): move from fish function to standalone bash script
    Replaces the fish function with a PATH-based bash script in ~/.local/bin.
    Solves stale function caching in long-lived shell sessions.
    Retains fish completions for flag discovery.

--- home/dot_config/fish/completions/e.fish
+++ home/dot_config/fish/completions/e.fish
@@ -1,3 +1,4 @@
+# Completions for e - edit files, optionally git-changed
 complete -c e -s g -l git -d 'All git-changed files (modified + untracked, excl. deleted/renamed)'
 complete -c e -s u -l untracked -d 'Untracked files'
 complete -c e -s m -l modified -d 'Modified files (unstaged)'
@@ -5,4 +6,4 @@ complete -c e -s s -l staged -d 'Staged files'
 complete -c e -s c -l conflicts -d 'Files with merge conflicts'
 complete -c e -s d -l deleted -d 'Deleted files'
 complete -c e -s r -l renamed -d 'Renamed files'
-complete -c e -s h -l help -d 'Show help'
+complete -c e -s h -l help -d 'Show this help'
